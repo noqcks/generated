ac22f46042217caede3b482292a3460d2ad0c062
Benji Visser <benji@093b.org>
2022-10-31 12:31:01 -0400

    Mirror github/linguist v7.23.0 (#22)
    * updates
    
    * fix tests
    
    * update CI
    
    * eslint
    
    * eslint test dir
    
    * tests passing
    
    Co-authored-by: Benji Visser <benny@noqcks.io>

--- test/helper.js
+++ test/helper.js
@@ -1,17 +1,46 @@
-var fs = require('fs');
-var path = require('path');
-const generated = require("../lib/generated");
+const fs = require('fs');
+const path = require('path');
+const Generated = require('../lib/generated');
 
-function isGenerated(name) {
-  filePath = path.join(__dirname + "/../samples", name);
-  try {
-    var contents = fs.readFileSync(filePath, 'utf8');
-  } catch (e) {
-    if (e.code !== 'ENOENT') throw err;
-    var contents = ''
-  }
-  g = new generated(name, contents)
-  return g.isGenerated()
+function fixturesPath() {
+  return path.join(__dirname, 'fixtures');
 }
 
-module.exports.isGenerated = isGenerated
+function samplesPath() {
+  return path.join(__dirname, '../samples');
+}
+
+function generatedLoadingData(name) {
+  const contents = fs.readFileSync(name, 'utf8');
+  const g = new Generated(name, contents);
+  return g.isGenerated();
+}
+
+function generatedWithoutLoadingData(name) {
+  const contents = '';
+  const g = new Generated(name, contents);
+  return g.isGenerated();
+}
+
+function generatedSampleLoadingData(name) {
+  return generatedLoadingData(path.join(samplesPath(), name));
+}
+
+function generatedSampleWithoutLoadingData(name) {
+  return generatedWithoutLoadingData(path.join(fixturesPath(), name));
+}
+
+function generatedFixtureWithoutLoadingData(name) {
+  return generatedWithoutLoadingData(path.join(fixturesPath(), name));
+}
+
+function generatedFixtureLoadingData(name) {
+  return generatedLoadingData(path.join(fixturesPath(), name));
+}
+
+module.exports = {
+  generatedSampleWithoutLoadingData,
+  generatedSampleLoadingData,
+  generatedFixtureLoadingData,
+  generatedFixtureWithoutLoadingData,
+};
